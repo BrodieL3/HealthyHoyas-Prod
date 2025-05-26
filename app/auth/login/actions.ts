9845654783174d42d5ca37fd728b4a70351dc810
Brodie Lee <brodieslee@gmail.com>
2025-05-26 17:36:15 -0400

    refresh the page on sign out

--- app/auth/login/actions.ts
+++ app/auth/login/actions.ts
@@ -50,6 +50,14 @@ export async function signOut() {
 
   const supabase = await createClient();
   const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    console.error("Error signing out:", error);
+    return;
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/auth/login");
 }
 
 export async function getUser() {
